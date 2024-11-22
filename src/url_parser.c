fe3d6b7a03a7b07e1b5621e161b1efcf69a8e48c
elliothacker33 <tomastorrespereira@gmail.com>
2024-11-22 11:26:47 +0000

    Merge errors

--- src/url_parser.c
+++ src/url_parser.c
@@ -5,6 +5,7 @@
 #define FTP_PREFIX_SIZE 6
 #define FTP_MAX_PORT 65535
 #define URL_MAX_PORT_LENGTH 15
+
 #define USER_ANONYMOUS "anonymous"
 #define PASS_ANONYMOUS "upXXXXXXXXX@edu.fe.up.pt" // Email address (optional)
 
